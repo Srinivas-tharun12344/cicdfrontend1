fdc63ca4b1ed8366d66207f9b58436ab39006889
Srinivas-tharun12344 <srinivasyenugu383@gmail.com>
2025-08-21 12:47:48 +0530

    first commit

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-export const BASEURL = "http://localhost:8080/cicd/";
+export const BASEURL = "http://localhost:8081/Calc/";
 export function callApi(reqmethod, url, data, responseHandler)
 {
     let options = "";
